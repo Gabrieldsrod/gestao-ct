cd6e643be8a3b28af8b0e9cea75ab53d603b0f58
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-02-14 14:00:13 -0300

    Adiciona classe abstrata de dados pessoais

---
+++ backend/src/main/java/com/gabrieldsrod/gestao_ct/Model/DadosPessoais.java
@@ -0,0 +1,26 @@
+package com.gabrieldsrod.gestao_ct.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@MappedSuperclass
+@Data
+public abstract class DadosPessoais {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(length = 20, nullable = false)
+    private String whatsapp;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+}
