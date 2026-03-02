e30b285e48386b455fcdfb25ad39510194cfd094
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-03-02 16:58:50 -0300

    Adiciona validações para os campos de nome, e-mail e WhatsApp no MemberRegistrationDTO

--- backend/src/main/java/com/gabrieldsrod/gestao_ct/DTO/request/MemberRegistrationDTO.java
+++ backend/src/main/java/com/gabrieldsrod/gestao_ct/DTO/request/MemberRegistrationDTO.java
@@ -1,5 +1,7 @@
 package com.gabrieldsrod.gestao_ct.DTO.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,8 +9,14 @@ import java.time.LocalDate;
 @Data
 public class MemberRegistrationDTO {
 
+    @NotBlank(message = "Nome não pode estar em branco")
     private String name;
+
+    @NotBlank(message = "E-mail não pode estar em branco")
+    @Email(message = "E-mail deve ser válido")
     private String email;
+
+    @NotBlank(message = "WhatsApp não pode estar em branco")
     private String whatsapp;
     private LocalDate birthDate;
     private Long planId;
