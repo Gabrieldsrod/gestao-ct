208f2f54c04d350e2110c4c48e55b17bde0aea02
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-03-09 00:04:32 -0300

    Corrige o mapeamento da URL no MemberController para incluir a barra inicial

--- backend/src/main/java/com/gabrieldsrod/gestao_ct/Controller/MemberController.java
+++ backend/src/main/java/com/gabrieldsrod/gestao_ct/Controller/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("v1/api/members")
+@RequestMapping("/v1/api/members")
 @CrossOrigin(origins = "*")
 public class MemberController {
 
