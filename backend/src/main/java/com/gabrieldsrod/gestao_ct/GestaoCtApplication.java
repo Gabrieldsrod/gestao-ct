eecbd4f3e1eb8d91338e62203f3d60857ec48d5c
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-03-13 12:57:11 -0300

    Configura o fuso horário padrão para America/Sao_Paulo na inicialização da aplicação

--- backend/src/main/java/com/gabrieldsrod/gestao_ct/GestaoCtApplication.java
+++ backend/src/main/java/com/gabrieldsrod/gestao_ct/GestaoCtApplication.java
@@ -1,10 +1,13 @@
 package com.gabrieldsrod.gestao_ct;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
@@ -13,6 +16,11 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @EnableJpaAuditing
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class GestaoCtApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoCtApplication.class, args);
